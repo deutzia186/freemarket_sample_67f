3233243f3a2f668aca282f72a0c130c86186296b
deutzia186 <samurai_tu186@hotmail.com>
2020-02-05 13:32:46 +0900

    update registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, :year_birth_at, :month_irth_at, :day_birth_at, :body ])
   end
-  
+
   private
   
   def production?

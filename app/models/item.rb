834636182a740b9b243c2c1f1c90f161c3e68aae
kishidashota <shidanemoaki@gmail.com>
2020-02-07 21:43:46 +0900

    master

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  # belongs_to :category
+  belongs_to :category
   belongs_to :seller, class_name: "User"
 
   extend ActiveHash::Associations::ActiveRecordExtensions

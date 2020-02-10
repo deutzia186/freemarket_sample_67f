b56c563d2eb9aad80dea6ca71d051ad5a5391b37
deutzia186 <samurai_tu186@hotmail.com>
2020-02-10 12:36:39 +0900

    update

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  belongs_to :category
+  # belongs_to :category
   belongs_to :seller, class_name: "User"
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -38,5 +38,4 @@ class Item < ApplicationRecord
 
     validates :seller_id,
       presence: true
-  
 end
